218f90ad331f85d71d319ea531a783d6278c0707
ffwff <mogamiriver@tuta.io>
2019-05-10 20:17:05 +0700

    add String::constructor

--- src/hanayo/mod.rs
+++ src/hanayo/mod.rs
@@ -15,19 +15,38 @@ pub fn init(vm : &mut Vm) {
         ($o: expr, $x:literal, $y:expr) => ($o.insert($x.to_string(), $y.wrap()));
     }
     // constants
+    set_var!("nil", Value::Nil);
     set_var!("true", Value::Int(1));
     set_var!("false", Value::Int(0));
     set_var!("inf", Value::Float(std::f64::INFINITY));
-    set_var!("nil", Value::Float(std::f64::NAN));
+    set_var!("nan", Value::Float(std::f64::NAN));
 
     // builtin functions
     set_var!("print", Value::NativeFn(io::print));
     set_var!("input", Value::NativeFn(io::input));
 
     // builtin objects
+    { // array
     let mut array : CHashMap = std::collections::HashMap::new();
     set_obj_var!(array, "constructor", Value::NativeFn(array::constructor));
     set_obj_var!(array, "length",      Value::NativeFn(array::length));
     set_obj_var!(array, "delete!",     Value::NativeFn(array::delete_));
+    set_obj_var!(array, "push",        Value::NativeFn(array::push));
+    set_obj_var!(array, "pop",         Value::NativeFn(array::pop));
+    set_obj_var!(array, "sort",        Value::NativeFn(array::sort));
+    set_obj_var!(array, "sort!",       Value::NativeFn(array::sort_));
+    set_obj_var!(array, "map",         Value::NativeFn(array::map));
+    set_obj_var!(array, "filter",      Value::NativeFn(array::filter));
+    set_obj_var!(array, "reduce",      Value::NativeFn(array::reduce));
+    set_obj_var!(array, "index",       Value::NativeFn(array::index));
+    set_obj_var!(array, "join",        Value::NativeFn(array::join));
     set_var!("Array", Value::Dict(unsafe{ &*Box::into_raw(Box::new(array)) }));
+    }
+
+    { // string
+    let mut string : CHashMap = std::collections::HashMap::new();
+    set_obj_var!(string, "constructor", Value::NativeFn(string::constructor));
+    set_var!("Array", Value::Dict(unsafe{ &*Box::into_raw(Box::new(string)) }));
+    }
+
 }
--- src/hanayo/string.rs
+++ src/hanayo/string.rs
@@ -2,8 +2,17 @@ use crate::vmbindings::vm::Vm;
 use crate::vmbindings::carray::CArray;
 use crate::vmbindings::cnativeval::NativeValue;
 use crate::vm::Value;
+use super::{malloc, drop};
 
-#[hana_function()]
-fn length(array: Value::Array) -> Value {
-    Value::Int(array.len() as i64)
+pub extern fn constructor(cvm : *mut Vm, nargs : u16) {
+    let vm = unsafe { &mut *cvm };
+    if nargs == 0 {
+        vm.stack.push(unsafe { Value::Str(&*malloc(String::new, |ptr| drop::<String>(ptr))) }.wrap());
+        return;
+    } else {
+        assert_eq!(nargs, 1);
+        let arg = vm.stack.top().clone();
+        vm.stack.pop();
+        vm.stack.push(unsafe { Value::Str(&*malloc(format!("{}", arg).to_string(), |ptr| drop::<String>(ptr))) }.wrap());
+    }
 }
