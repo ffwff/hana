8f078a7a9a6796f0695f9eea31a7608bdfa56201
ffwff <mogamiriver@tuta.io>
2019-05-14 13:32:06 +0700

    box => astbox for span storage [wip]

--- src/ast.rs
+++ src/ast.rs
@@ -17,6 +17,18 @@ pub mod ast {
         fn as_any(&self) -> &dyn Any;
         fn emit(&self, c : &mut compiler::Compiler);
     }
+    pub struct AstBox {
+        pub data: std::boxed::Box<AST>,
+        pub start: usize,
+        pub end: usize
+    }
+
+    pub fn pos_to_line(input: &str, pos: usize) -> (usize, usize) {
+        let before = &input[..pos];
+        let line = before.as_bytes().iter().filter(|&&c| c == b'\n').count() + 1;
+        let col = before.chars().rev().take_while(|&c| c != '\n').count() + 1;
+        (line, col)
+    }
 
     // # values
     // ## identifier
--- src/main.rs
+++ src/main.rs
@@ -16,6 +16,23 @@ pub use vmbindings::vm::VmOpcode;
 pub use vmbindings::gc::set_root;
 mod hanayo;
 
+fn print_error<T>(s: &String, lineno: usize, col: usize, etype: &str, message: T)
+    where T: std::fmt::Debug {
+    let line = s.split("\n").nth(lineno-1).unwrap();
+    let lineno_info = format!("{} | ", lineno);
+    let lineno_info_len = lineno_info.len();
+    eprintln!("
+{}{}
+{}
+
+{} {:?}",
+    ac::Blue.bold().paint(lineno_info),
+    line,
+    ac::Blue.bold().paint(" ".repeat(lineno_info_len + col-1) + "^"),
+    ac::Red.bold().paint(etype.to_string() + ":"),
+    message);
+}
+
 fn main() {
     let args : Vec<String> = std::env::args().collect();
     let mut file = std::fs::File::open(&args[1]).unwrap_or_else(|err| {
@@ -28,19 +45,7 @@ fn main() {
         std::process::exit(1);
     });
     let prog = ast::grammar::start(&s).unwrap_or_else(|err| {
-        let line = s.split("\n").nth(err.line-1).unwrap();
-        let lineno_info = format!("{} | ", err.line);
-        let lineno_info_len = lineno_info.len();
-        eprintln!("
-{}{}
-{}
-
-{} expected {:?}",
-    ac::Blue.bold().paint(lineno_info),
-    line,
-    ac::Blue.bold().paint(" ".repeat(lineno_info_len + err.column-1) + "^"),
-    ac::Red.bold().paint("parser error:"),
-    err.expected);
+        print_error(&s, err.line, err.column, "parser error", err.expected);
         std::process::exit(1);
     });
     let mut c = ManuallyDrop::new(compiler::Compiler::new());
