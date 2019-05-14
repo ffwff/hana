07f904a17cb8eeeaa4884c1bb9071694ec76a932
ffwff <mogamiriver@tuta.io>
2019-05-14 12:46:20 +0700

    print out error messages when parser fails

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,11 @@
 #![feature(alloc_layout_extra)]
 #![feature(ptr_offset_from)]
 
-#[macro_use] extern crate decorator;
 use std::io::Read;
 use std::mem::ManuallyDrop;
+#[macro_use] extern crate decorator;
+extern crate ansi_term;
+use ansi_term::Color as ac;
 
 pub mod compiler;
 pub mod ast;
@@ -16,10 +18,31 @@ mod hanayo;
 
 fn main() {
     let args : Vec<String> = std::env::args().collect();
-    let mut file = std::fs::File::open(&args[1]).unwrap();
+    let mut file = std::fs::File::open(&args[1]).unwrap_or_else(|err| {
+        println!("error opening file: {}", err);
+        std::process::exit(1);
+    });
     let mut s = String::new();
-    file.read_to_string(&mut s).unwrap();
-    let prog = ast::grammar::start(&s).unwrap();
+    file.read_to_string(&mut s).unwrap_or_else(|err| {
+        println!("error reading file: {}", err);
+        std::process::exit(1);
+    });
+    let prog = ast::grammar::start(&s).unwrap_or_else(|err| {
+        let line = s.split("\n").nth(err.line-1).unwrap();
+        let lineno_info = format!("{} | ", err.line);
+        let lineno_info_len = lineno_info.len();
+        eprintln!("
+{}{}
+{}
+
+{} expected {:?}",
+    ac::Blue.bold().paint(lineno_info),
+    line,
+    ac::Blue.bold().paint(" ".repeat(lineno_info_len + err.column-1) + "^"),
+    ac::Red.bold().paint("parser error:"),
+    err.expected);
+        std::process::exit(1);
+    });
     let mut c = ManuallyDrop::new(compiler::Compiler::new());
     // manually drop because the interpreter will exit early
     for stmt in prog {
