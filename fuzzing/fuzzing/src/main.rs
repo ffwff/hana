a88a2e312b4b3043e83f968a6e8ee998c6c9ff93
ffwff <mogamiriver@tuta.io>
2019-06-01 13:13:20 +0700

    update fuzzing utils

--- fuzzing/fuzzing/src/main.rs
+++ fuzzing/fuzzing/src/main.rs
@@ -8,26 +8,20 @@ use haru::hanayo;
 use haru::vmbindings::value::Value;
 use haru::vmbindings::vm::{Vm, VmOpcode};
 
-macro_rules! eval {
-    ($x:expr) => {{
-        let prog = grammar::start($x).unwrap();
-        let mut c = compiler::Compiler::new();
-        for stmt in prog {
-            stmt.emit(&mut c);
-        }
-        c.cpushop(VmOpcode::OP_HALT);
-        let mut vm = c.into_vm();
-        hanayo::init(&mut vm);
-        vm.gc_enable();
-        vm.execute();
-        vm
-    }};
-}
-
 fn main() {
     fuzz!(|data: &[u8]| {
         if let Ok(s) = std::str::from_utf8(data) {
-            eval!(s);
+            if let Ok(prog) = grammar::start(s) {
+                let mut c = compiler::Compiler::new();
+                for stmt in prog {
+                    stmt.emit(&mut c);
+                }
+                c.cpushop(VmOpcode::OP_HALT);
+                let mut vm = c.into_vm();
+                hanayo::init(&mut vm);
+                vm.gc_enable();
+                vm.execute();
+            }
         }
     });
 }
